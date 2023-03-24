b90c776e4e0a7160ba02c622a32a9e4ff261f3a4
Mikoz999 <mikozotieno@gmail.com>
2023-03-24 12:23:36 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
-#include "main.h"
+#include "main.h"
 
 /**
- * print_numbers - print 0 to 9
- * owned by Bwave ICT
- * Return: Void
+ * print_numbers - print 0-9
+ * return: Void
  */
 
-void print_numbers(void)
+void print_numbers(void)
 {
-	int c;
+	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = '0', cu='9', c++)
 	{
-		_putchar(c+ '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
