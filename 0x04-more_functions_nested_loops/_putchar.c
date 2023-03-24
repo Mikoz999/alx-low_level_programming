2b10afbe99b00a150a93881bd8408b7089dd5a58
Mikoz999 <mikozotieno@gmail.com>
2023-03-24 12:04:32 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar.c - writes the charcter c tobstdout
+ * @c the chararacter print
+ *
+ * Return: On succes 0 on error -1
+ *
+ */
+
+int _putchar(char c)
+{
+	return (write(, &c, 1));
+}
