5496c931730bd8489e53a2d4ef082159511c9084
Mikoz999 <mikozotieno@gmail.com>
2023-03-27 08:08:33 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,10 @@
 
 /**
  * resst_to_98 - Takes a pointer to an int as parameter and updates the value it points to to 98.
- * Return: nothing
+ * 
+ * @n: An interger input pointer
  *
+ * Return : nothing
  */
 void reset_to_98(int *n)
 {
