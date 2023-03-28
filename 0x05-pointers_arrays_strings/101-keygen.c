80c9811c287bb9c12cb668dc4a95b7ffd0dc5a5a
Mikoz999 <mikozotieno@gmail.com>
2023-03-28 08:00:31 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,24 +9,9 @@
  */
 
 int main(int argc, char *argv[]) {
-    srand(time(NULL));
-
-    /* Generate password with a sum of characters equal to 2772 */
-    int sum = 0;
-    while (sum < 2772) {
-        int r = rand() % 62;
-        if (r < 10) {
-            putchar('0' + r);
-            sum += r;
-        } else if (r < 36) {
-            putchar('a' + r - 10);
-            sum += r - 10;
-        } else {
-            putchar('A' + r - 36);
-            sum += r - 36;
-        }
-    }
-    putchar('\n');
+    int sum;
+    sum = 0;
+    printf("Keygen\n");
     return 0;
 }
 
