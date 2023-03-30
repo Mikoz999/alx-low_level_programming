70a20ae3a737ceb50176cdc55ff39cd350657945
Mikoz999 <mikozotieno@gmail.com>
2023-03-30 13:50:13 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,59 +1,23 @@
-#include "main.h"
-/**
- * infinite_add - adds two numbers.
- * @n1: first number
- * @n2: second number
- * @r: store result
- * @size_r: size of stored result
- *
- * Return: pointer to r, or NULL if result would overflow
- */
+#include <stdio.h>
 
-char *infinite_add(const char *n1, const char *n2, char *r, int size_r)
+int main(void)
 {
-    int i, j, k, l, m, n, carry = 0;
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
-    int maxlen = (len1 > len2) ? len1 : len2;
+    int n;
+    int a[5];
+    int *p;
 
-    if (maxlen + 1 > size_r)
-        return NULL;
-
-    r[maxlen] = '\0';
-
-    for (i = len1 - 1, j = len2 - 1, k = maxlen - 1; k >= 0; i--, j--, k--)
-    {
-        n = carry;
-        if (i >= 0)
-            n += n1[i] - '0';
-        if (j >= 0)
-            n += n2[j] - '0';
-
-        if (n >= 10)
-        {
-            carry = 1;
-            n -= 10;
-        }
-        else
-        {
-            carry = 0;
-        }
-
-        r[k] = n + '0';
-    }
-
-    if (carry)
-    {
-        if (maxlen + 2 > size_r)
-            return NULL;
-
-        for (l = maxlen; l > 0; l--)
-        {
-            r[l] = r[l - 1];
-        }
-        r[0] = '1';
-    }
-
-    return r;
+    a[2] = 1024;
+    p = &n;
+    /* 
+     * write your line of code here... 
+     * Remember: 
+     * - you are not allowed to use a 
+     * - you are not allowed to modify p 
+     * - only one statement 
+     * - you are not allowed to code anything else than this line of code 
+     */ 
+    *(a + 2) = 98;
+    /* ...so that this prints 98\n */ 
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
-
