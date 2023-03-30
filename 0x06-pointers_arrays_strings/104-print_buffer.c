23d6bc413ec866ee291e2288bedd02ed07a39b3b
Mikoz999 <mikozotieno@gmail.com>
2023-03-30 14:35:31 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,53 +1,63 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * print_buffer - function that prints a buffer.
- * @buffer: pointer to the buffer
+ * print_line - prints a line of a buffer in hexadecimal and ASCII format
+ * @buffer: buffer to print from
+ * @size: size of the buffer
+ * @line_num: line number to print
+ */
+void print_line(char *buffer, int size, int line_num)
+{
+    int i, j;
+
+    for (i = line_num * 10; i < line_num * 10 + 10; i++)
+    {
+        if (i < size)
+        {
+            putchar((buffer[i] >> 4) & 0xf ? (buffer[i] >> 4) + '0' + 7 + ('A' - '9' - 1) : (buffer[i] >> 4) + '0');
+            putchar((buffer[i] & 0xf) ? (buffer[i] & 0xf) + '0' + 7 + ('A' - '9' - 1) : (buffer[i] & 0xf) + '0');
+        }
+        else
+        {
+            printf("  ");
+        }
+
+        if (i % 2 != 0)
+        {
+            putchar(' ');
+        }
+    }
+
+    putchar(' ');
+
+    for (j = line_num * 10; j < line_num * 10 + 10 && j < size; j++)
+    {
+        if (buffer[j] >= 32 && buffer[j] <= 126)
+        {
+            putchar(buffer[j]);
+        }
+        else
+        {
+            putchar('.');
+        }
+    }
+
+    putchar('\n');
+}
+
+/**
+ * print_buffer - prints a buffer in hexadecimal and ASCII format
+ * @buffer: buffer to print
  * @size: size of the buffer
  */
 void print_buffer(char *buffer, int size)
 {
-    int bufferIndex = 0, lineIndex;
+    int i, num_lines;
 
-    if (size <= 0) {
-        putchar('\n');
-    } else {
-        while (bufferIndex < size) {
-            // Print the index of the current line
-            printf("%08x: ", bufferIndex);
-
-            // Print the hexadecimal representation of the current line
-            lineIndex = 0;
-            while (lineIndex < 10) {
-                if (lineIndex % 2 == 0 && lineIndex > 0) {
-                    printf(" ");
-                }
-                if (lineIndex + bufferIndex > size - 1) {
-                    printf("  ");
-                } else {
-                    printf("%.2x", buffer[lineIndex + bufferIndex]);
-                }
-                lineIndex++;
-            }
-
-            // Print the ASCII representation of the current line
-            putchar(' ');
-            lineIndex = 0;
-            while (lineIndex < 10) {
-                if (lineIndex + bufferIndex > size - 1) {
-                    break;
-                }
-                if (buffer[lineIndex + bufferIndex] >= ' ' && buffer[lineIndex + bufferIndex] <= '~') {
-                    putchar(buffer[lineIndex + bufferIndex]);
-                } else {
-                    putchar('.');
-                }
-                lineIndex++;
-            }
-
-            putchar('\n');
-            bufferIndex += 10;
-        }
+    for (i = 0, num_lines = size / 10 + (size % 10 != 0); i < num_lines; i++)
+    {
+        printf("%08x: ", i * 10);
+        print_line(buffer, size, i);
     }
 }
-
