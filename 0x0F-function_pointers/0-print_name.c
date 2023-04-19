a9d9437a10ac522d24e099a4f4becae81e8a80a0
Mikoz999 <mikozotieno@gmail.com>
2023-04-19 07:37:40 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ *
+ * @f - function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
+
