9c66c808e4536ca7b2e0d6ea74b123e6d1946970
Mikoz999 <mikozotieno@gmail.com>
2023-04-19 09:51:20 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * int_index - searches for an integer in an array of integers.
- *
- * @array: array of integers
+ * int_index - searches for an integer in an array of integers
+ * @array: pointer to the array of integers
  * @size: size of the array
  * @cmp: pointer to the function to be used to compare values
  *
- * Return: index of the first element for which cmp function does not return 0
- *         -1 if no element matches or if size <= 0
+ * Return: index of the first element for which the cmp function does not return 0,
+ * -1 if no element matches or size is less than or equal to 0.
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
     int i;
 
-    /* iterate over the array */
-    for (i = 0; i < size; i++)
-    {
-        /* call cmp function with each element of the array */
-        if (cmp(array[i]))
-        {
-            /* return index of element if cmp returns nonzero value */
+    /* check if array and cmp function pointers are not NULL, and size is greater than 0 */
+    if (array == NULL || cmp == NULL || size <= 0)
+        return (-1);
+
+    /* loop through the array */
+    for (i = 0; i < size; i++) {
+        /* call the cmp function with each element */
+        if (cmp(array[i])) {
+            /* if cmp returns non-zero, return the index of the element */
             return i;
         }
     }
-
-    /* return -1 if no element matches or if size <= 0 */
+    /* if no element matches, return -1 */
     return -1;
 }
 
