9fc7ed30579fa2ff98544b06033fe92bc80c5535
Mikoz999 <mikozotieno@gmail.com>
2023-05-02 13:26:10 -0700

    function that prints a listint_t

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,42 +1,54 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: Pointer to the head of the linked list.
+ * print_listint_safe - Prints a listint_t linked list safely
+ * @head: Pointer to the head node of the list
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow, *fast;
-	size_t count = 0;
+    const listint_t *slow, *fast;
+    size_t count = 0;
 
-	if (head == NULL)
-		return (0);
+    if (head == NULL)
+        return (0);
 
-	slow = head;
-	fast = head;
+    slow = head;
+    fast = head->next;
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
+    while (fast != NULL && fast->next != NULL && fast != slow)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        slow = slow->next;
+        fast = fast->next->next;
+        count++;
+    }
 
-		slow = slow->next;
-		fast = fast->next->next;
+    if (fast != NULL && fast->next != NULL)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        printf("-> [%p] %d\n", (void *)fast, fast->n);
+        count++;
+    }
+    else
+    {
+        while (slow != NULL)
+        {
+            printf("[%p] %d\n", (void *)slow, slow->n);
+            slow = slow->next;
+            count++;
+        }
+    }
 
-		if (slow == fast)
-		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			exit(98);
-		}
-	}
+    if (fast != NULL && fast->next != NULL)
+    {
+        printf("-> [%p] %d\n", (void *)fast, fast->n);
+        exit(98);
+    }
 
-	printf("[%p] %d\n", (void *)slow, slow->n);
-	count++;
-
-	return (count);
+    return (count);
 }
 
