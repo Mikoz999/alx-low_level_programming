ef258fd11dc55c987dd2e8556f8f7a3adbfcc8c1
Mikoz999 <mikozotieno@gmail.com>
2023-06-19 13:50:18 -0700

    adding a Return statement in the comment and an if statement

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,16 +2,21 @@
 #include "dog.h"
 
 /**
- * init_dog - Initialize a variable of type struct dog
+ * init_dog - Initializes a dog structure
  * @d: Pointer to the struct dog variable to initialize
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
+ *
+ * Return: None
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+	   d->name = name;
+	   d->age = age;
+	   d->owner = owner;
+	}
 }
 
