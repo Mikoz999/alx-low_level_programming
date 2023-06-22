8cfa4d36991a22938da60775525cd084eef4403a
Mikoz999 <mikozotieno@gmail.com>
2023-06-22 16:29:15 -0700

    3-print_all.c:

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,47 +2,6 @@
 #include <stdarg.h>
 
 /**
- * print_char - Prints a character.
- * @args: The va_list of arguments.
- */
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_integer - Prints an integer.
- * @args: The va_list of arguments.
- */
-void print_integer(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - Prints a float.
- * @args: The va_list of arguments.
- */
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - Prints a string.
- * @args: The va_list of arguments.
- */
-void print_string(va_list args)
-{
-	char *str = va_arg(args, char *);
-
-	if (str != NULL)
-		printf("%s", str);
-	else
-		printf("(nil)");
-}
-
-/**
  * print_all - Prints anything based on the format specifier.
  * @format: A list of types of arguments passed to the function.
  */
@@ -50,42 +9,45 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char current_format;
+	char *str;
+	char current_char;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		current_format = format[i];
+		current_char = format[i];
 
-		switch (current_format)
+		switch (current_char)
 		{
 			case 'c':
-				print_char(args);
+				printf("%c", va_arg(args, int));
 				break;
 			case 'i':
-				print_integer(args);
+				printf("%d", va_arg(args, int));
 				break;
 			case 'f':
-				print_float(args);
+				printf("%f", va_arg(args, double));
 				break;
 			case 's':
-				print_string(args);
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
 				break;
 			default:
 				i++;
 				continue;
 		}
 
-		i++;
-
-		if (format[i] && (current_format == 'c' || current_format == 'i' ||
-				  current_format == 'f' || current_format == 's'))
+		if (format[i + 1])
 			printf(", ");
+
+		i++;
 	}
 
-	printf("\n");
-
 	va_end(args);
+
+	printf("\n");
 }
 
