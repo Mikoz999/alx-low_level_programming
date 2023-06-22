f6e674e92c7dcdb329bb14da26192c8b4ad6a908
Mikoz999 <mikozotieno@gmail.com>
2023-06-21 19:36:20 -0700

    adding some comments and fixing the mistackes identified by the checker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,6 +4,16 @@
 /**
  * print_opcodes - Prints the opcodes of the main function
  * @num_bytes: Number of bytes to print
+ *
+ * Description: This function prints the opcodes of its own main function
+ *              by reading the memory at the corresponding address.
+ *              It prints the opcodes in lowercase hexadecimal format,
+ *              with each opcode represented by two characters.
+ *              The listing ends with a newline character.
+ *              If the number of bytes is negative, it prints an error message
+ *              and exits with status 2. If the number of arguments
+ *              is incorrect,
+ *              it prints an error message and exits with status 1.
  */
 void print_opcodes(int num_bytes)
 {
@@ -20,6 +30,7 @@ void print_opcodes(int num_bytes)
 	{
 		printf("%02x ", ptr[i]);
 	}
+
 	printf("\n");
 }
 
@@ -27,7 +38,15 @@ void print_opcodes(int num_bytes)
  * main - Entry point of the program
  * @argc: Number of command-line arguments
  * @argv: Array of command-line arguments
- * Return: 0 on success, 1 on invalid argument count
+ *
+ * Description: This function is the entry point of the program.
+ *              It checks if the correct number of arguments is provided.
+ *              If not, it prints an error message and exits with status 1.
+ *              It converts the argument to an integer using atoi.
+ *              Then, it calls the print_opcodes function with the provided
+ *              number of bytes to print the opcodes of its own main function.
+ *
+ * Return: 0 on success, 1 on incorrect argument count
  */
 int main(int argc, char *argv[])
 {
@@ -38,6 +57,7 @@ int main(int argc, char *argv[])
 	}
 
 	int num_bytes = atoi(argv[1]);
+
 	print_opcodes(num_bytes);
 
 	return (0);
