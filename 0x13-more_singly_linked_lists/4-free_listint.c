6ea2353ae2876b3b0167e8857e485d9c446446af
Mikoz999 <mikozotieno@gmail.com>
2023-07-03 19:36:20 -0700

    Implemented the free_listint function according to the Betty coding style guidelines

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_listint - Frees a listint_t list.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
