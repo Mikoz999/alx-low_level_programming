fea0ec08232433158ae737467aa491c1ae6765a1
Mikoz999 <mikozotieno@gmail.com>
2023-08-07 04:46:38 -0700

    Fix coding style issues in 0-read_textfile.c The changes include fixing code indentation by using tabs where possible, removing leading spaces at the start of lines, and ensuring lines are not longer than 80 characters.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,8 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to
+ * the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
@@ -13,39 +14,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, read_count, write_count;
-    char *buffer;
+	int fd, read_count, write_count;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    read_count = read(fd, buffer, letters);
-    if (read_count == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	read_count = read(fd, buffer, letters);
+	if (read_count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    write_count = write(STDOUT_FILENO, buffer, read_count);
-    if (write_count == -1 || read_count != write_count)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	if (write_count == -1 || read_count != write_count)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
+	free(buffer);
+	close(fd);
 
-    return (write_count);
+	return (write_count);
 }
 
